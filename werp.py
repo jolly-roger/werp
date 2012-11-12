8c029d5726c424c9ef4eaf9ad224f5e079350c61
roger <ionyshchenko@sequans.com>
2012-11-12 11:57:59 +0200

    i

--- werp.py
+++ werp.py
@@ -2,6 +2,7 @@ from cherrypy.process import servers, plugins
 from cherrypy import wsgiserver
 import cherrypy
 import os.path
+import datetime
 
 
 from errors import errors
@@ -10,11 +11,21 @@ from robots import robots
 from picpuk import picpuk
 
 
+WERP_ACCESS_LOG_FILE = '/home/www/werp_access.log'
+werp_access_log_file = open(WERP_ACCESS_LOG_FILE, 'a')
+
+def werp_access_log():
+    werp_access_log_file.write(str(cherrypy.request.base) + ' ' + cherrypy.request.request_line + ' ' + datetime.now() + \
+        ' ' + str(cherrypy.request.headers))
+
+cherrypy.tools.werp_access_log = cherrypy.Tool('on_start_resource', werp_access_log)
+
 def fake_wait_for_occupied_port(host, port): return
 servers.wait_for_occupied_port = fake_wait_for_occupied_port
 
 cherrypy.config.update({'tools.sessions.on': True,
     'tools.sessions.timeout': 30,
+    'tools.werp_access_log.on': True,
     'server.thread_pool': 111,
     'server.socket_queue_size': 33,
     'log.access_file': '/home/www/access.log',
