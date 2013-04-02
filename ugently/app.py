209f2934ad3b25521fbff54c163b98e507f14da5
roger <ionyshchenko@sequans.com>
2013-04-02 16:08:16 +0300

    i

--- ugently/app.py
+++ ugently/app.py
@@ -1,5 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+import traceback
+
 from werp import orm
+
+from . import layout
 
 def app(env, start_res):
     start_res('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
-    return bytes(str(env), 'utf-8')
+    l = bytes('', 'utf-8')
+    try:
+        conn = orm.q_engine.connect()
+        ses = orm.sescls(bind=conn)
+        user_agents = ses.query(orm.UserAgent).all()
+        l = layout.getNome(user_agents)
+        ses.close()
+        conn.close()
+    except:
+        sender = 'www@dig-dns.com (www)'
+        recipient = 'roger@dig-dns.com'
+    
+        msg = MIMEText(traceback.format_exc())
+        msg['Subject'] = 'ugently error'
+        msg['From'] = sender
+        msg['To'] = recipient
+    
+        s = smtplib.SMTP('localhost')
+        s.sendmail(sender, recipient, msg.as_string())
+        s.quit()
+    return l
