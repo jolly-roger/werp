76707c0d3ba23d7f9e30312dbd43fb3ab6b31c57
roger <ionyshchenko@sequans.com>
2013-04-02 17:45:47 +0300

    i

--- ugently/layout/layout.py
+++ ugently/layout/layout.py
@@ -3,12 +3,12 @@ from jinja2 import Environment, FileSystemLoader
 import gettext
 import configparser
 
-config = configparser.ConfigParser()
-config.read('/home/www/ugently/app.conf')
+#config = configparser.ConfigParser()
+#config.read('/home/www/ugently/app.conf')
 
 def getenv(lng='EN'):
-    if lng == 'RU':
-        return ru_env
+ #   if lng == 'RU':
+  #      return ru_env
     return en_env
 
 def getHome(user_agents, lng='EN'):
@@ -18,11 +18,11 @@ def getHome(user_agents, lng='EN'):
 #trans_ru_RU = gettext.translation('messages', languages=['ru_RU'], localedir=config['global']['layout.trans'])
 #trans_en_US = gettext.translation('messages', languages=['en_US'], localedir=config['global']['layout.trans'])
 
-ru_env = Environment(loader = FileSystemLoader(config['global']['layout.templates']))#, extensions=['jinja2.ext.i18n'])
+#ru_env = Environment(loader = FileSystemLoader(config['global']['layout.templates']))#, extensions=['jinja2.ext.i18n'])
 #ru_env.install_gettext_translations(trans_ru_RU)
 #ru_env.globals['ru_domain'] = config['global']['domains.ru']
 #ru_env.globals['en_domain'] = config['global']['domains.en']
-en_env = Environment(loader = FileSystemLoader(config['global']['layout.templates']))#, extensions=['jinja2.ext.i18n'])
+en_env = Environment(loader = FileSystemLoader('/home/www/ugently/layout/templates'))#, extensions=['jinja2.ext.i18n'])
 #en_env.install_gettext_translations(trans_en_US)
 #en_env.globals['ru_domain'] = config['global']['domains.ru']
 #en_env.globals['en_domain'] = config['global']['domains.en']
