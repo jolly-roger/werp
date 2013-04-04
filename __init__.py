6efde2be3bb04738604c8eac4194b9c695fa5bca
roger <ionyshchenko@sequans.com>
2013-04-04 17:24:41 +0300

    i

--- __init__.py
+++ __init__.py
@@ -1,14 +1,6 @@
 import logging
 import logging.handlers
 
-class WerpLogger(logging.Logger):
-    def __init__(self, *a, **kw):
-        super().__init__(*a, **kw)
-    def _log(self, level, msg, args, subj = 'werp notification', **kw):
-        record = super()._log(level, msg, args, **kw)
-        record.subj = subj
-        return record
-   
 class WerpSMTPHandler(logging.handlers.SMTPHandler):
     def __init__(self, *a, **kw):
         super().__init__(*a, **kw)
@@ -18,7 +10,7 @@ class WerpSMTPHandler(logging.handlers.SMTPHandler):
         else:
             return super().getSubject(record)
 
-nlog = WerpLogger('werp_notification')
+nlog = logging.getLogger('werp_notification')
 nlog.setLevel(logging.DEBUG)
 nlog_fh = WerpSMTPHandler('localhost', 'www@dig-dns.com (www)', 'roger@dig-dns.com', 'werp notification')
 nlog_fh.setLevel(logging.DEBUG)
