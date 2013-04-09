d46113882f860b8d332e89435f5f321fc01608d0
roger <ionyshchenko@sequans.com>
2013-04-09 09:57:05 +0300

    i

--- orm/base.py
+++ orm/base.py
@@ -2,5 +2,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, BigInteger, SmallInteger, Date, DateTime, Boolean, ForeignKey, and_, Text
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import relationship, backref, column_property
+from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import datetime
+
+from ..common import etype
 
 DBase = declarative_base()
--- orm/session.py
+++ orm/session.py
@@ -1,7 +1,9 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
 
+null_engine = create_engine('postgresql://werp:0v}II587@localhost:5432/werp', poolclass=NullPool)
 q_engine = create_engine('postgresql://werp:0v}II587@localhost:5432/werp', pool_size=1024, max_overflow=0,
 	pool_recycle=3600)
 sescls = sessionmaker()
