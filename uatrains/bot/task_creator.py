15d26eabc382dc586f1f1dedae39b33676ada11d
roger <ionyshchenko@sequans.com>
2013-04-09 14:10:52 +0300

    i

--- uatrains/bot/task_creator.py
+++ uatrains/bot/task_creator.py
@@ -1,6 +1,6 @@
 import traceback
 
-from werp import orm
+from werp.orm import uatrains
 from werp import nlog
 from werp.uatrains.engine import drv
 
@@ -9,23 +9,23 @@ try:
     ses = orm.sescls(bind=conn)
     
     for tid in range(0, 5000):
-        ua_bot_task = orm.uatrains.BotTask()
+        ua_bot_task = uatrains.BotTask()
         ua_bot_task.data = drv.southwest.ua_url.replace('(tid)', str(tid))
         ses.add(ua_bot_task)
-        ru_bot_task = orm.uatrains.BotTask()
+        ru_bot_task = uatrains.BotTask()
         ru_bot_task.data = drv.southwest.ru_url.replace('(tid)', str(tid))
         ses.add(ru_bot_task)
-        en_bot_task = orm.uatrains.BotTask()
+        en_bot_task = uatrains.BotTask()
         en_bot_task.data = drv.southwest.en_url.replace('(tid)', str(tid))
         ses.add(en_bot_task)
     for tid in range(20000, 70000):
-        ua_bot_task = orm.uatrains.BotTask()
+        ua_bot_task = uatrains.BotTask()
         ua_bot_task.data = drv.passengers.ua_url.replace('(tid)', str(tid))
         ses.add(ua_bot_task)
-        ru_bot_task = orm.uatrains.BotTask()
+        ru_bot_task = uatrains.BotTask()
         ru_bot_task.data = drv.passengers.ru_url.replace('(tid)', str(tid))
         ses.add(ru_bot_task)
-        en_bot_task = orm.uatrains.BotTask()
+        en_bot_task = uatrains.BotTask()
         en_bot_task.data = drv.passengers.en_url.replace('(tid)', str(tid))
         ses.add(en_bot_task)
     ses.commit()
