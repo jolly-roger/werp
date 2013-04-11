6bd2484a92c5dbff977749abca811ad39e6ee637
roger <ionyshchenko@sequans.com>
2013-04-11 16:36:47 +0300

    i

--- froxly/http_request_worker.py
+++ froxly/http_request_worker.py
@@ -21,32 +21,35 @@ worker.send_unicode(LRU_READY)
 
 cycles = 0
 while True:
-    msg = worker.recv_multipart()
-    
-    print(msg)
-    
-    if not msg:
-        break
-    conn = orm.q_engine.connect()
-    ses = orm.sescls(bind=conn)
-    url = msg[2].decode('utf-8')
-    res = ''
-    try_count = 0
-    while res is '' and try_count < 11:
-        proxies = ses.query(orm.FreeProxy).filter(orm.FreeProxy.protocol == 'http').all()
-        user_agents = ses.query(orm.UserAgent).filter(orm.UserAgent.is_bot == False).all()
-        rnd_proxy = random.choice(proxies)
-        rnd_user_agent = random.choice(user_agents)
-        req = urllib.request.Request(url, headers={'User-Agent': rnd_user_agent.value})
-        req.set_proxy(rnd_proxy.ip + ':' + rnd_proxy.port, rnd_proxy.protocol)
-        try:
-            res = urllib.request.urlopen(req)
-            if res.getcode() != 200:
+    try:
+        msg = worker.recv_multipart()
+        if not msg:
+            break
+        conn = orm.q_engine.connect()
+        ses = orm.sescls(bind=conn)
+        url = msg[2].decode('utf-8')
+        
+        print(url)
+        
+        res = ''
+        try_count = 0
+        while res is '' and try_count < 11:
+            proxies = ses.query(orm.FreeProxy).filter(orm.FreeProxy.protocol == 'http').all()
+            user_agents = ses.query(orm.UserAgent).filter(orm.UserAgent.is_bot == False).all()
+            rnd_proxy = random.choice(proxies)
+            rnd_user_agent = random.choice(user_agents)
+            req = urllib.request.Request(url, headers={'User-Agent': rnd_user_agent.value})
+            req.set_proxy(rnd_proxy.ip + ':' + rnd_proxy.port, rnd_proxy.protocol)
+            try:
+                res = urllib.request.urlopen(req)
+                if res.getcode() != 200:
+                    res = ''
+            except:
+                nlog.info('froxly - zw', traceback.format_exc())
                 res = ''
-        except:
-            nlog.info('froxly - grabber error - request data', traceback.format_exc())
-            res = ''
-        try_count = try_count + 1
-    ses.close()
-    conn.close()
-    worker.send_multipart(msg)
+            try_count = try_count + 1
+        ses.close()
+        conn.close()
+        worker.send_multipart(msg)
+    except:
+        nlog.info('froxly - zw', traceback.format_exc())
