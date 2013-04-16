8da96280408ce022a5682066dab99fa11725b0c3
roger <ionyshchenko@sequans.com>
2013-04-16 17:25:54 +0300

    i

--- froxly/checker.py
+++ froxly/checker.py
@@ -24,6 +24,7 @@ try:
             msg = 'get'
             rnd_user_agent_socket.send_unicode(msg)
             rnd_user_agent = rnd_user_agent_socket.recv_unicode()
+            nlog.info('froxly - checker request debug', rnd_user_agent)
             req = urllib.request.Request(test_url, headers={'User-Agent': rnd_user_agent})
             req.set_proxy(proxy.ip + ':' + proxy.port, proxy.protocol)
             try:
