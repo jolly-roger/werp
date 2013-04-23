aefc672fcfc8bd5831b5107e1234a56789df2a1e
roger <ionyshchenko@sequans.com>
2013-04-23 14:41:15 +0300

    i

--- froxly/errors.py
+++ froxly/errors.py
@@ -1,6 +1,6 @@
 class ProxyError(Exception):
-    def __init__(self, proxy=None, base_exception=None):
-        self.proxy = proxy
+    def __init__(self, base_exception=None, proxy=None):
         self.base_exception = base_exception
+        self.proxy = proxy
     def __str__(self):
         return str(self.base_exception)
