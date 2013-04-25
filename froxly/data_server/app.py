830ff8aeccbd8b3f6bdda73dd304f86143d68146
roger <ionyshchenko@sequans.com>
2013-04-25 16:15:25 +0300

    i

--- froxly/data_server/app.py
+++ froxly/data_server/app.py
@@ -9,8 +9,7 @@ from werp import orm
 from werp import nlog
 from werp.common import sockets
 from werp.common import red_keys
-
-from . import checker
+from werp.froxly.data_server import checker
 
 expire_delta = datetime.timedelta(days=1)
 conn = None
