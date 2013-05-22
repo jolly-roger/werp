d630463b8fc52e296e5b0c20043d33660f9f5701
roger <ionyshchenko@sequans.com>
2013-05-22 14:40:48 +0300

    i

--- froxly/data_server/app.py
+++ froxly/data_server/app.py
@@ -18,8 +18,8 @@ try:
     ctx = zmq.Context()
     froxly_data_server_socket = ctx.socket(zmq.REP)
     froxly_data_server_socket.bind(sockets.froxly_data_server)
-    froxly_checker_server_socket = ctx.socket(zmq.PUSH)
-    froxly_checker_server_socket.bind(sockets.froxly_checker_server)
+    #froxly_checker_server_socket = ctx.socket(zmq.PUSH)
+    #froxly_checker_server_socket.bind(sockets.froxly_checker_server)
     red = redis.StrictRedis(unix_socket_path=sockets.redis)
     def rnd(msg):
         rnd_free_proxy = None
@@ -62,11 +62,11 @@ try:
         
         nlog.info('froxly - data server info', '1')
         
-        froxly_checker_server_socket.send_unicode(json.dumps(msg))
+        #froxly_checker_server_socket.send_unicode(json.dumps(msg))
         froxly_data_server_socket.send_unicode(json.dumps({'result': None}))
     def list_for_url(msg):
         msg['method'] = 'url_check'
-        froxly_checker_server_socket.send_unicode(json.dumps(msg))
+        #froxly_checker_server_socket.send_unicode(json.dumps(msg))
         froxly_data_server_socket.send_unicode(json.dumps({'result': None}))
     def deactivate_for_url(msg):
         if 'proxy' in msg['params'] and 'url' in msg['params']:
