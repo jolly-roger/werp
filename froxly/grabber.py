80572dc09f1dba79e9c0cbfeabd4f6f52ccc4cb0
roger <ionyshchenko@sequans.com>
2013-05-23 17:47:18 +0300

    i

--- froxly/grabber.py
+++ froxly/grabber.py
@@ -74,33 +74,36 @@ try:
     dom_tree = etree.parse(io.StringIO(res_data), html_parser)
     raw_proxies = dom_tree.xpath('/html/body/div/div/table/tr')
     for raw_proxy in raw_proxies:
-        raw_ip = etree.tostring(raw_proxy[1]).decode('utf-8')
-        raw_ip_style_text = raw_proxy[1].xpath("span/style/text()")
-        ip_dn_classes = re.findall("\.([^\{]+)\{display:\s*none\}", raw_ip_style_text[0])
-        for ip_dn_class in ip_dn_classes:
-            ip_dn_class_divs = re.findall('<div\s*class="' + ip_dn_class + '">\d*</div>', raw_ip)
-            for ip_dn_class_div in ip_dn_class_divs:
-                raw_ip = raw_ip.replace(ip_dn_class_div, '')
-            ip_dn_class_spans = re.findall('<span\s*class="' + ip_dn_class + '">\d*</span>', raw_ip)
-            for ip_dn_class_span in ip_dn_class_spans:
-                raw_ip = raw_ip.replace(ip_dn_class_span, '')
-        ip_dn_divs = re.findall('<div\s*style="display:\s*none">\d*</div>', raw_ip)
-        for ip_dn_div in ip_dn_divs:
-            raw_ip = raw_ip.replace(ip_dn_div, '')
-        ip_dn_spans = re.findall('<span\s*style="display:\s*none">\d*</span>', raw_ip)
-        for ip_dn_span in ip_dn_spans:
-            raw_ip = raw_ip.replace(ip_dn_span, '')
-        ip_parts = re.findall('>\s*([\.0-9]+)\s*<', raw_ip)
-        fp = orm.FreeProxy()
-        fp.ip = ''.join(ip_parts)
-        fp.port = raw_proxy[2].text.strip()
-        fp.protocol = raw_proxy[6].text.lower().strip()
         try:
-            ses.query(orm.FreeProxy).filter(orm.and_(orm.and_(orm.FreeProxy.ip == fp.ip, orm.FreeProxy.port == fp.port),
-                orm.FreeProxy.protocol == fp.protocol)).one()
-        except orm.NoResultFound:
-            ses.add(fp)
-            ses.commit()
+            raw_ip = etree.tostring(raw_proxy[1]).decode('utf-8')
+            raw_ip_style_text = raw_proxy[1].xpath("span/style/text()")
+            ip_dn_classes = re.findall("\.([^\{]+)\{display:\s*none\}", raw_ip_style_text[0])
+            for ip_dn_class in ip_dn_classes:
+                ip_dn_class_divs = re.findall('<div\s*class="' + ip_dn_class + '">\d*</div>', raw_ip)
+                for ip_dn_class_div in ip_dn_class_divs:
+                    raw_ip = raw_ip.replace(ip_dn_class_div, '')
+                ip_dn_class_spans = re.findall('<span\s*class="' + ip_dn_class + '">\d*</span>', raw_ip)
+                for ip_dn_class_span in ip_dn_class_spans:
+                    raw_ip = raw_ip.replace(ip_dn_class_span, '')
+            ip_dn_divs = re.findall('<div\s*style="display:\s*none">\d*</div>', raw_ip)
+            for ip_dn_div in ip_dn_divs:
+                raw_ip = raw_ip.replace(ip_dn_div, '')
+            ip_dn_spans = re.findall('<span\s*style="display:\s*none">\d*</span>', raw_ip)
+            for ip_dn_span in ip_dn_spans:
+                raw_ip = raw_ip.replace(ip_dn_span, '')
+            ip_parts = re.findall('>\s*([\.0-9]+)\s*<', raw_ip)
+            fp = orm.FreeProxy()
+            fp.ip = ''.join(ip_parts)
+            fp.port = raw_proxy[2].text.strip()
+            fp.protocol = raw_proxy[6].text.lower().strip()
+            try:
+                ses.query(orm.FreeProxy).filter(orm.and_(orm.and_(orm.FreeProxy.ip == fp.ip, orm.FreeProxy.port == fp.port),
+                    orm.FreeProxy.protocol == fp.protocol)).one()
+            except orm.NoResultFound:
+                ses.add(fp)
+                ses.commit()
+        except:
+            nlog.info('froxly - grabber error', traceback.format_exc() + '\n\n' + str(raw_proxy))
     ses.close()
     conn.close()
     end_time = time.time()
