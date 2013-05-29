efe2fa538149fe6582e3629f5ee556ece39b8e0d
roger <ionyshchenko@sequans.com>
2013-05-29 15:14:36 +0300

    i

--- common/timeouts.py
+++ common/timeouts.py
@@ -1,4 +1,4 @@
 froxly_grabber = 7.0
 froxly_checker = 7.0
-froxly_requester = 7.0
+froxly_requester = 5.0
 uatrains_bot = 7.0
--- froxly/data_server/app.py
+++ froxly/data_server/app.py
@@ -14,14 +14,28 @@ WORKER_POOL = 32
 
 try:
     ctx = zmq.Context()
-    froxly_data_server_socket = ctx.socket(zmq.REP)
+    
+    # data server frontend socket
+    froxly_data_server_socket = ctx.socket(zmq.ROUTER)
     froxly_data_server_socket.bind(sockets.froxly_data_server)
+    
+    # data server worker backend socket
+    froxly_data_worker_socket = ctx.socket(zmq.DEALER)
+    froxly_data_worker_socket.bind(sockets.froxly_data_worker)
+    
+    # requester server backend socket
+    froxly_requester_server_socket = ctx.socket(zmq.DEALER)
+    froxly_requester_server_socket.bind(sockets.froxly_requester_server)
+    
+    # checker server backend socket
     froxly_checker_server_socket = ctx.socket(zmq.PUSH)
     froxly_checker_server_socket.connect(sockets.froxly_checker_server)
-    froxly_data_worker_socket = ctx.socket(zmq.REQ)
-    froxly_data_worker_socket.bind(sockets.froxly_data_worker)
-    froxly_requester_server_socket = ctx.socket(zmq.REQ)
-    froxly_requester_server_socket.bind(sockets.froxly_requester_server)
+    
+    poller = zmq.Poller()
+    poller.register(froxly_data_server_socket, zmq.POLLIN)
+    poller.register(froxly_data_worker_socket, zmq.POLLIN)
+    poller.register(froxly_requester_server_socket, zmq.POLLIN)
+
     def check(msg):
         msg['method'] = 'base_check'
         froxly_checker_server_socket.send_unicode(json.dumps(msg))
@@ -51,28 +65,41 @@ try:
             froxly_data_worker_socket.send_unicode(json.dumps(deactivate_proxy_req))
             froxly_data_worker_socket.recv_unicode()
         froxly_data_server_socket.send_unicode(worker_res_msg)
+        
     for wrk_num in range(WORKER_POOL):
         thr = threading.Thread(target=worker.run)
         thr.start()
     checker_methods = {}
-    checker_methods[check.__name__] = check
-    checker_methods[list_for_url.__name__] = list_for_url
+    #checker_methods[check.__name__] = check
+    #checker_methods[list_for_url.__name__] = list_for_url
     requester_methods = {}
-    requester_methods[request.__name__] = request
+    #requester_methods[request.__name__] = request
+    
     while True:
+        sockets = dict(poller.poll())
         try:
-            req_msg = froxly_data_server_socket.recv_unicode()
-            msg = json.loads(req_msg)
-            if msg['method'] in checker_methods:
-                checker_methods[msg['method']](msg)
-            elif msg['method'] in requester_methods:
-                requester_methods[msg['method']](msg)
-            else:
-                froxly_data_worker_socket.send_unicode(req_msg)
-                res_msg = froxly_data_worker_socket.recv_unicode()
-                froxly_data_server_socket.send_unicode(res_msg)
+            # data server frontend
+            if froxly_data_server_socket in sockets and  sockets[froxly_data_server_socket] == zmq.POLLIN:
+                req_msg = froxly_data_server_socket.recv_multipart()
+                msg = json.loads(req_msg[2].decode())
+                if msg['method'] in checker_methods:
+                    checker_methods[msg['method']](msg)
+                elif msg['method'] in requester_methods:
+                    requester_methods[msg['method']](msg)
+                else:
+                    froxly_data_worker_socket.send_multipart(message)
+            
+            # data server worker backend
+            if froxly_data_worker_socket in sockets and  sockets[froxly_data_worker_socket] == zmq.POLLIN:
+                res_msg = froxly_data_worker_socket.recv_multipart()
+                froxly_data_server_socket.send_multipart(res_msg)
+            
+            # requester server backend
+            if froxly_requester_server_socket in sockets and  sockets[froxly_requester_server_socket] == zmq.POLLIN:
+                res_msg = froxly_requester_server_socket.recv_multipart()
+                froxly_data_server_socket.send_multipart(res_msg) 
         except:
-            froxly_data_server_socket.send_unicode(json.dumps({'result': None}))
+            #froxly_data_server_socket.send_unicode(json.dumps({'result': None}))
             nlog.info('froxly - data server error', traceback.format_exc())
 except:
     nlog.info('froxly - data server fatal', traceback.format_exc())
