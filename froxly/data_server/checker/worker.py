3320b4b7265b51b8c8a305543c50e40e9364c0fa
roger <ionyshchenko@sequans.com>
2013-06-03 12:01:50 +0300

    i

--- froxly/data_server/checker/worker.py
+++ froxly/data_server/checker/worker.py
@@ -4,6 +4,7 @@ import traceback
 import zmq
 import json
 import socket
+import struct
 
 from werp import nlog
 from werp.common import sockets
@@ -24,46 +25,81 @@ def run():
             ugently_data_server_socket.send_unicode('')
             rnd_user_agent = ugently_data_server_socket.recv_unicode()
             try:
-                is_1_1 = False
-                
-                # HTTP 1.1
-                s = socket.socket()
-                s.settimeout(timeouts.froxly_checker)
-                url_obj = urllib.parse.urlparse(task['url'])
-                s.connect((task['proxy']['ip'], int(task['proxy']['port'])))
-                req_str = 'GET ' + task['url'] + ' HTTP/1.1\r\nHost:' + url_obj.netloc + '\r\n\r\n'
-                s.sendall(req_str.encode())
-                res = s.recv(15).decode()
-                s.close()
-                if res == 'HTTP/1.1 200 OK':
-                    is_1_1 = True
-                    task['proxy']['http_status'] = 200
-                    task['proxy']['http_status_reason'] = None
-                    task['proxy']['protocol_version'] = '1.1'
-                else:
-                    task['proxy']['http_status'] = -1
-                    task['proxy']['http_status_reason'] = res
-                
-                # HTTP 1.0
-                if not is_1_1:
+                if task['proxy']['porotocol'] == 'socks':
                     s = socket.socket()
                     s.settimeout(timeouts.froxly_checker)
                     url_obj = urllib.parse.urlparse(task['url'])
                     s.connect((task['proxy']['ip'], int(task['proxy']['port'])))
-                    req_str = 'GET ' + task['url'] + ' HTTP/1.0\r\nHost:' + url_obj.netloc + '\r\n\r\n'
+                    ipaddr = socket.inet_aton(socket.gethostbyname(url_obj.netloc))
+                    destport = 80
+                    conn_req = struct.pack(">BBH", 0x04, 0x01, destport) + ipaddr
+                    conn_req = conn_req + chr(0x00).encode()
+                    s.sendall(conn_req)
+                    conn_resp = s.recv(8)
+                    if resp[0:1] == chr(0x00).encode() or resp[1:2] == chr(0x5A).encode():
+                        s.close()
+                        raise Exception('Socks proxy error')
+                    req_str = 'GET ' + task['url'] + ' HTTP/1.1\r\nHost:' + url_obj.netloc + '\r\n\r\n'
                     s.sendall(req_str.encode())
                     res = s.recv(15).decode()
                     s.close()
-                    if res == 'HTTP/1.0 200 OK':
+                    if res == 'HTTP/1.0 200 OK' or res == 'HTTP/1.1 200 OK':
                         task['proxy']['http_status'] = 200
                         task['proxy']['http_status_reason'] = None
-                        task['proxy']['protocol_version'] = '1.0'
+                        task['proxy']['protocol_version'] = '4'
                     else:
                         task['proxy']['http_status'] = -1
                         task['proxy']['http_status_reason'] = res
+                else:
+                    is_1_1 = False
+                    
+                    # HTTP 1.1
+                    s = socket.socket()
+                    s.settimeout(timeouts.froxly_checker)
+                    url_obj = urllib.parse.urlparse(task['url'])
+                    s.connect((task['proxy']['ip'], int(task['proxy']['port'])))
+                    req_str = 'GET ' + task['url'] + ' HTTP/1.1\r\nHost:' + url_obj.netloc + '\r\n\r\n'
+                    s.sendall(req_str.encode())
+                    res = s.recv(15).decode()
+                    s.close()
+                    if res == 'HTTP/1.1 200 OK':
+                        is_1_1 = True
+                        task['proxy']['http_status'] = 200
+                        task['proxy']['http_status_reason'] = None
+                        task['proxy']['protocol_version'] = '1.1'
+                    else:
+                        task['proxy']['http_status'] = -1
+                        task['proxy']['http_status_reason'] = res
+                    
+                    # HTTP 1.0
+                    if not is_1_1:
+                        s = socket.socket()
+                        s.settimeout(timeouts.froxly_checker)
+                        url_obj = urllib.parse.urlparse(task['url'])
+                        s.connect((task['proxy']['ip'], int(task['proxy']['port'])))
+                        req_str = 'GET ' + task['url'] + ' HTTP/1.0\r\nHost:' + url_obj.netloc + '\r\n\r\n'
+                        s.sendall(req_str.encode())
+                        res = s.recv(15).decode()
+                        s.close()
+                        if res == 'HTTP/1.0 200 OK':
+                            task['proxy']['http_status'] = 200
+                            task['proxy']['http_status_reason'] = None
+                            task['proxy']['protocol_version'] = '1.0'
+                        else:
+                            task['proxy']['http_status'] = -1
+                            task['proxy']['http_status_reason'] = res
             except Exception as e:
                 task['proxy']['http_status'] = -1
                 task['proxy']['http_status_reason'] = str(e)
             froxly_checker_res.send_unicode(json.dumps(task))
     except:
         nlog.info('froxly - checher error', traceback.format_exc())
+
+def socks_recvall(self, count):
+    data = self.recv(count)
+    while len(data) < count:
+        d = self.recv(count-len(data))
+        if not d:
+            raise GeneralProxyError((0, "connection closed unexpectedly"))
+        data = data + d
+    return data
--- froxly/data_server/requester/worker.py
+++ froxly/data_server/requester/worker.py
@@ -27,8 +27,6 @@ def run():
             req_url = None
             res = {'result': {'data': None, 'http_status': None, 'http_status_reason': None}}
             try:
-                s = socket.socket()
-                s.settimeout(timeouts.froxly_requester)
                 req = json.loads(req_msg)
                 url_obj = urllib.parse.urlparse(req['params']['url'])
                 ugently_data_server_socket.send_unicode('')
@@ -39,27 +37,64 @@ def run():
                     rnd_proxy_req['params'] = {'url': req_url}
                 froxly_data_server_socket.send_unicode(json.dumps(rnd_proxy_req))
                 rnd_proxy = json.loads(froxly_data_server_socket.recv_unicode())['result']
-                s.connect((rnd_proxy['ip'], int(rnd_proxy['port'])))
-                remote_req_str = 'GET ' + req['params']['url'] + ' ' + rnd_proxy['protocol_version'] + \
-                    '\r\nHost:' + url_obj.netloc + '\r\n\r\n'
-                s.sendall(remote_req_str.encode())
-                remote_charset = 'utf-8'
-                if 'charset' in req['params'] and req['params']['charset'] is not None:
-                    remote_charset = req['params']['charset']
-                remote_res = s.recv(15).decode(remote_charset, 'ignore')
-                if remote_res == 'HTTP/1.1 200 OK' or remote_res == 'HTTP/1.0 200 OK':
-                    buf = s.recv(1024)
-                    while buf:
-                        remote_res += buf.decode(remote_charset, 'ignore')
+                if task['proxy']['porotocol'] == 'socks':
+                    s = socket.socket()
+                    s.settimeout(timeouts.froxly_checker)
+                    url_obj = urllib.parse.urlparse(task['url'])
+                    s.connect((task['proxy']['ip'], int(task['proxy']['port'])))
+                    ipaddr = socket.inet_aton(socket.gethostbyname(url_obj.netloc))
+                    destport = 80
+                    conn_req = struct.pack(">BBH", 0x04, 0x01, destport) + ipaddr
+                    conn_req = conn_req + chr(0x00).encode()
+                    s.sendall(conn_req)
+                    conn_resp = s.recv(8)
+                    if resp[0:1] == chr(0x00).encode() or resp[1:2] == chr(0x5A).encode():
+                        s.close()
+                        raise Exception('Socks proxy error')
+                    remote_req_str = 'GET ' + req['params']['url'] + ' ' + rnd_proxy['protocol_version'] + \
+                        '\r\nHost:' + url_obj.netloc + '\r\n\r\n'
+                    s.sendall(remote_req_str.encode())
+                    remote_charset = 'utf-8'
+                    if 'charset' in req['params'] and req['params']['charset'] is not None:
+                        remote_charset = req['params']['charset']
+                    remote_res = s.recv(15).decode(remote_charset, 'ignore')
+                    if remote_res == 'HTTP/1.1 200 OK' or remote_res == 'HTTP/1.0 200 OK':
                         buf = s.recv(1024)
-                    start_body = remote_res.find('\r\n\r\n')
-                    res['result']['data'] = remote_res[start_body + 4:]
-                    res['result']['http_status'] = 200
-                    res['result']['http_status_reason'] = None
+                        while buf:
+                            remote_res += buf.decode(remote_charset, 'ignore')
+                            buf = s.recv(1024)
+                        start_body = remote_res.find('\r\n\r\n')
+                        res['result']['data'] = remote_res[start_body + 4:]
+                        res['result']['http_status'] = 200
+                        res['result']['http_status_reason'] = None
+                    else:
+                        res['result']['http_status'] = -1
+                        res['result']['http_status_reason'] = remote_res
+                    s.close()
                 else:
-                    res['result']['http_status'] = -1
-                    res['result']['http_status_reason'] = remote_res
-                s.close()
+                    s = socket.socket()
+                    s.settimeout(timeouts.froxly_requester)
+                    s.connect((rnd_proxy['ip'], int(rnd_proxy['port'])))
+                    remote_req_str = 'GET ' + req['params']['url'] + ' ' + rnd_proxy['protocol_version'] + \
+                        '\r\nHost:' + url_obj.netloc + '\r\n\r\n'
+                    s.sendall(remote_req_str.encode())
+                    remote_charset = 'utf-8'
+                    if 'charset' in req['params'] and req['params']['charset'] is not None:
+                        remote_charset = req['params']['charset']
+                    remote_res = s.recv(15).decode(remote_charset, 'ignore')
+                    if remote_res == 'HTTP/1.1 200 OK' or remote_res == 'HTTP/1.0 200 OK':
+                        buf = s.recv(1024)
+                        while buf:
+                            remote_res += buf.decode(remote_charset, 'ignore')
+                            buf = s.recv(1024)
+                        start_body = remote_res.find('\r\n\r\n')
+                        res['result']['data'] = remote_res[start_body + 4:]
+                        res['result']['http_status'] = 200
+                        res['result']['http_status_reason'] = None
+                    else:
+                        res['result']['http_status'] = -1
+                        res['result']['http_status_reason'] = remote_res
+                    s.close()
             except Exception as e:
                 res['result']['http_status'] = -11
                 res['result']['http_status_reason'] = str(e)
