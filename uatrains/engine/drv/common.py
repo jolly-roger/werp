916a7730817c76481775239593a76b7916562469
roger <ionyshchenko@sequans.com>
2013-06-19 14:47:07 +0300

    i

--- uatrains/engine/drv/common.py
+++ uatrains/engine/drv/common.py
@@ -18,11 +18,15 @@ def has_all_data(e):
 def get_t(e, ses):
 	t = None
 	try:
+		prepared_ua_title = e.ua_title if e.ua_title is not None else ''
+		prepared_ru_title = e.ru_title if e.ru_title is not None else ''
+		prepared_en_title = e.en_title if e.en_title is not None else ''
 		t = ses.query(orm.E).\
 			filter(orm.and_(orm.E.etype == e.etype, orm.E.oid == e.oid,
 				orm.E.value == e.value)).\
-			filter(orm.or_(orm.E.ua_title == e.ua_title, orm.E.ru_title == e.ru_title,
-				orm.E.en_title == e.en_title)).\
+			filter(orm.or_(orm.E.ua_title == prepared_ua_title,
+				orm.E.ru_title == prepared_ru_title,
+				orm.E.en_title == prepared_en_title)).\
 			one()
 	except orm.NoResultFound:
 		pass
@@ -32,9 +36,9 @@ def get_t(e, ses):
 def get_s(e, ses):
 	s = None
 	try:
-		prepared_ua_title = e.ua_title.replace(' ', '%').replace('-', '%')
-		prepared_ru_title = e.ru_title.replace(' ', '%').replace('-', '%')
-		prepared_en_title = e.en_title.replace(' ', '%').replace('-', '%')
+		prepared_ua_title = e.ua_title.replace(' ', '%').replace('-', '%') if e.ua_title is not None else ''
+		prepared_ru_title = e.ru_title.replace(' ', '%').replace('-', '%') if e.ru_title is not None else ''
+		prepared_en_title = e.en_title.replace(' ', '%').replace('-', '%') if e.en_title is not None else ''
 		s = ses.query(orm.E).\
 			filter(orm.and_(orm.E.etype == e.etype, orm.E.oid == e.oid)).\
 			filter(orm.or_(orm.E.ua_title.ilike(prepared_ua_title),
