0e388632d2201f6894fe5219ddbfd58ef437c261
roger <ionyshchenko@sequans.com>
2013-07-18 12:52:10 +0300

    i

--- uatrains/uatrains.py
+++ uatrains/uatrains.py
@@ -290,7 +290,12 @@ class uatrains(object):
         conn.close()
         return layout.getTrains(ts, ph, pn, has_next_p, lng)
     @cherrypy.expose
-    def es(self, ph='', pn=0):
+    def es(self, ph=None, fs=None, ts=None, pn=0):
+        
+        
+        logger.info('es ph: \'' + str(ph) + '\'')
+        
+        
         lng = get_lng()
         conn = orm.q_engine.connect()
         ses = orm.sescls(bind=conn)
@@ -298,28 +303,81 @@ class uatrains(object):
         pc = 9
         es = []
         has_next_p = False
-        prepared_ph = ph.replace(' ', '%').replace('-', '%')
-        try:
-            q = ses.query(orm.uatrains.E).\
-                filter(orm.and_(orm.or_(orm.uatrains.E.ua_title.ilike('%' + prepared_ph.lower() + '%'),
-                    orm.uatrains.E.ru_title.ilike('%' + prepared_ph.lower() + '%'),
-                    orm.uatrains.E.en_title.ilike('%' + prepared_ph.lower() + '%'),
-                    orm.uatrains.E.ua_graph.ilike('%' + prepared_ph.lower() + '%'),
-                    orm.uatrains.E.ru_graph.ilike('%' + prepared_ph.lower() + '%'),
-                    orm.uatrains.E.en_graph.ilike('%' + prepared_ph.lower() + '%'),
-                    orm.uatrains.E.value.op('similar to')('([0-9А-Яа-я]*/)?' + prepared_ph.lower() + \
-                        '([А-Яа-я]*)?(/[0-9А-Яа-я]*)?(/[0-9А-Яа-я]*)?')),
-                    orm.uatrains.E.ref_id == None)).\
-                order_by(orm.uatrains.E.etype.desc(), orm.uatrains.E.vc.desc(), orm.uatrains.E.ua_title)
-            es = q.limit(pc).offset(pn * pc).all()
-            next_p_es = q.limit(pc).offset((pn + 1) * pc).all()
-            if len(next_p_es) > 0:
-                has_next_p = True
-        except Exception:
-            nlog.info('Uatrains error', 'Can\'t find entities\n' + traceback.format_exc())
+        if ph is not None:
+            prepared_ph = ph.replace(' ', '%').replace('-', '%')
+            try:
+                q = ses.query(orm.uatrains.E).\
+                    filter(orm.and_(orm.or_(orm.uatrains.E.ua_title.ilike('%' + prepared_ph.lower() + '%'),
+                        orm.uatrains.E.ru_title.ilike('%' + prepared_ph.lower() + '%'),
+                        orm.uatrains.E.en_title.ilike('%' + prepared_ph.lower() + '%'),
+                        orm.uatrains.E.ua_graph.ilike('%' + prepared_ph.lower() + '%'),
+                        orm.uatrains.E.ru_graph.ilike('%' + prepared_ph.lower() + '%'),
+                        orm.uatrains.E.en_graph.ilike('%' + prepared_ph.lower() + '%'),
+                        orm.uatrains.E.value.op('similar to')('([0-9А-Яа-я]*/)?' + prepared_ph.lower() + \
+                            '([А-Яа-я]*)?(/[0-9А-Яа-я]*)?(/[0-9А-Яа-я]*)?')),
+                        orm.uatrains.E.ref_id == None)).\
+                    order_by(orm.uatrains.E.etype.desc(), orm.uatrains.E.vc.desc(), orm.uatrains.E.ua_title)
+                es = q.limit(pc).offset(pn * pc).all()
+                next_p_es = q.limit(pc).offset((pn + 1) * pc).all()
+                if len(next_p_es) > 0:
+                    has_next_p = True
+            except Exception:
+                nlog.info('Uatrains error', 'Can\'t find entities\n' + traceback.format_exc())
+        elif fs is not None and ts is not None:
+            prepared_fs = fs.replace(' ', '%').replace('-', '%')
+            prepared_ts = ts.replace(' ', '%').replace('-', '%')
+            prepared_ph = prepared_fs + '%' + prepared_ts
+            try:
+                q = ses.query(orm.uatrains.E).\
+                    filter(orm.and_(
+                        orm.or_(orm.uatrains.E.ua_graph.ilike('%' + prepared_ph.lower() + '%'),
+                            orm.uatrains.E.ru_graph.ilike('%' + prepared_ph.lower() + '%'),
+                            orm.uatrains.E.en_graph.ilike('%' + prepared_ph.lower() + '%')),
+                        orm.uatrains.E.ref_id == None)).\
+                    order_by(orm.uatrains.E.etype.desc(), orm.uatrains.E.vc.desc(), orm.uatrains.E.ua_title)
+                es = q.limit(pc).offset(pn * pc).all()
+                next_p_es = q.limit(pc).offset((pn + 1) * pc).all()
+                if len(next_p_es) > 0:
+                    has_next_p = True
+            except Exception:
+                nlog.info('Uatrains error', 'Can\'t find entities by fs and ts\n' + traceback.format_exc())
+        elif fs is not None and ts is None:
+            prepared_fs = fs.replace(' ', '%').replace('-', '%')
+            prepared_ph = prepared_fs
+            try:
+                q = ses.query(orm.uatrains.E).\
+                    filter(orm.and_(
+                        orm.or_(orm.uatrains.E.ua_graph.ilike('%' + prepared_ph.lower() + '%'),
+                            orm.uatrains.E.ru_graph.ilike('%' + prepared_ph.lower() + '%'),
+                            orm.uatrains.E.en_graph.ilike('%' + prepared_ph.lower() + '%')),
+                        orm.uatrains.E.ref_id == None)).\
+                    order_by(orm.uatrains.E.etype.desc(), orm.uatrains.E.vc.desc(), orm.uatrains.E.ua_title)
+                es = q.limit(pc).offset(pn * pc).all()
+                next_p_es = q.limit(pc).offset((pn + 1) * pc).all()
+                if len(next_p_es) > 0:
+                    has_next_p = True
+            except Exception:
+                nlog.info('Uatrains error', 'Can\'t find entities by fs\n' + traceback.format_exc())
+        elif fs is None and ts is not None:
+            prepared_ts = ts.replace(' ', '%').replace('-', '%')
+            prepared_ph = prepared_ts
+            try:
+                q = ses.query(orm.uatrains.E).\
+                    filter(orm.and_(
+                        orm.or_(orm.uatrains.E.ua_graph.ilike('%' + prepared_ph.lower() + '%'),
+                            orm.uatrains.E.ru_graph.ilike('%' + prepared_ph.lower() + '%'),
+                            orm.uatrains.E.en_graph.ilike('%' + prepared_ph.lower() + '%')),
+                        orm.uatrains.E.ref_id == None)).\
+                    order_by(orm.uatrains.E.etype.desc(), orm.uatrains.E.vc.desc(), orm.uatrains.E.ua_title)
+                es = q.limit(pc).offset(pn * pc).all()
+                next_p_es = q.limit(pc).offset((pn + 1) * pc).all()
+                if len(next_p_es) > 0:
+                    has_next_p = True
+            except Exception:
+                nlog.info('Uatrains error', 'Can\'t find entities by ts\n' + traceback.format_exc())
         ses.close()
         conn.close()
-        return layout.getEs(es, ph, pn, has_next_p, lng)
+        return layout.getEs(es, ph, fs, ts, pn, has_next_p, lng)
     @cherrypy.expose
     def news(self):
         lng = get_lng()
