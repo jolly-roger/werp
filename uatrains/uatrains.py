b55e7605d941b42dec29e5e3bd63086d14eb3e9e
roger <ionyshchenko@sequans.com>
2013-07-19 15:32:03 +0300

    i

--- uatrains/uatrains.py
+++ uatrains/uatrains.py
@@ -299,24 +299,7 @@ class uatrains(object):
             es, has_next_p = search.from_to(ses, fs, ts, pc, pn)
         elif int(srcht) == 1:
             prepared_ph = ph.replace(' ', '%').replace('-', '%')
-            try:
-                q = ses.query(orm.uatrains.E).\
-                    filter(orm.and_(orm.or_(orm.uatrains.E.ua_title.ilike('%' + prepared_ph.lower() + '%'),
-                        orm.uatrains.E.ru_title.ilike('%' + prepared_ph.lower() + '%'),
-                        orm.uatrains.E.en_title.ilike('%' + prepared_ph.lower() + '%'),
-                        orm.uatrains.E.ua_graph.ilike('%' + prepared_ph.lower() + '%'),
-                        orm.uatrains.E.ru_graph.ilike('%' + prepared_ph.lower() + '%'),
-                        orm.uatrains.E.en_graph.ilike('%' + prepared_ph.lower() + '%'),
-                        orm.uatrains.E.value.op('similar to')('([0-9А-Яа-я]*/)?' + prepared_ph.lower() + \
-                            '([А-Яа-я]*)?(/[0-9А-Яа-я]*)?(/[0-9А-Яа-я]*)?')),
-                        orm.uatrains.E.ref_id == None)).\
-                    order_by(orm.uatrains.E.etype.desc(), orm.uatrains.E.vc.desc(), orm.uatrains.E.ua_title)
-                es = q.limit(pc).offset(pn * pc).all()
-                next_p_es = q.limit(pc).offset((pn + 1) * pc).all()
-                if len(next_p_es) > 0:
-                    has_next_p = True
-            except Exception:
-                nlog.info('Uatrains error', 'Can\'t find entities\n' + traceback.format_exc())
+            es, has_next_p = search.full(ses, ph, pc, pn)
         ses.close()
         conn.close()
         return layout.getEs(es, srcht, ph, fs, ts, pn, has_next_p, lng)
