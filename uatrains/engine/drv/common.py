b2705b4c85d6324702c54ed310cde6afb17829aa
roger <ionyshchenko@sequans.com>
2013-07-22 12:42:35 +0300

    i

--- uatrains/engine/drv/common.py
+++ uatrains/engine/drv/common.py
@@ -5,7 +5,7 @@ from werp import orm
 
 def is_not_empty(e):
 	ret = False
-	if (e.ua_title is not None or e.ru_title is not None or e.en_title is not None) and e.value is not None and \
+	if (e.ua_title is not None or e.ru_title is not None) and e.value is not None and \
 		e.oid is not None:
 		ret = True
 	return ret
@@ -13,7 +13,6 @@ def has_all_data(e):
 	ret = False
 	if e.ua_title is not None and \
 		e.ru_title is not None and \
-		e.en_title is not None and \
 		e.value is not None and \
 		e.oid is not None:
 		ret = True
@@ -23,13 +22,11 @@ def get_t(e, ses):
 	try:
 		prepared_ua_title = e.ua_title if e.ua_title is not None else ''
 		prepared_ru_title = e.ru_title if e.ru_title is not None else ''
-		prepared_en_title = e.en_title if e.en_title is not None else ''
 		t = ses.query(orm.uatrains.E).\
 			filter(orm.and_(orm.uatrains.E.etype == e.etype, orm.uatrains.E.oid == e.oid,
 				orm.uatrains.E.value == e.value,
 				orm.or_(orm.uatrains.E.ua_title == prepared_ua_title,
-					orm.uatrains.E.ru_title == prepared_ru_title,
-					orm.uatrains.E.en_title == prepared_en_title))).\
+					orm.uatrains.E.ru_title == prepared_ru_title))).\
 			one()
 	except orm.NoResultFound:
 		pass
@@ -41,13 +38,11 @@ def get_s(e, ses):
 	try:
 		prepared_ua_title = e.ua_title.replace(' ', '%').replace('-', '%') if e.ua_title is not None else ''
 		prepared_ru_title = e.ru_title.replace(' ', '%').replace('-', '%') if e.ru_title is not None else ''
-		prepared_en_title = e.en_title.replace(' ', '%').replace('-', '%') if e.en_title is not None else ''
 		s = ses.query(orm.uatrains.E).\
 			filter(orm.and_(orm.uatrains.E.etype == e.etype,
 				orm.or_(orm.uatrains.E.oid == e.oid, e.oid > 20000, orm.uatrains.E.oid > 20000),
 				orm.or_(orm.uatrains.E.ua_title.ilike(prepared_ua_title),
-					orm.uatrains.E.ru_title.ilike(prepared_ru_title),
-					orm.uatrains.E.en_title.ilike(prepared_en_title)))).\
+					orm.uatrains.E.ru_title.ilike(prepared_ru_title)))).\
 			one()
 	except orm.NoResultFound:
 		pass
