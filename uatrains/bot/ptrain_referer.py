f98926cf869945eb5dd9c60a0c79df7ef1f5c049
roger <ionyshchenko@sequans.com>
2013-07-23 12:55:15 +0300

    i

--- uatrains/bot/ptrain_referer.py
+++ uatrains/bot/ptrain_referer.py
@@ -11,7 +11,7 @@ try:
     start_time = time.time()
     conn = orm.null_engine.connect()
     ses = orm.sescls(bind=conn)
-    etrains = ses.query(orm.uatrains.E).filter(orm.or_(orm.uatrains.E.etype == 1, orm.uatrains.E.etype == 4)).all()
+    etrains = ses.query(orm.uatrains.E).filter(orm.uatrains.E.etype == 5).all()
     for et in etrains:
         if et.ref_id is None:
             if (et.from_date is None and et.to_date is None) or \
@@ -20,17 +20,12 @@ try:
                     et.to_date >= datetime.datetime.now())):
                 last_etrain = et
                 similar_etrains = ses.query(orm.uatrains.E).\
-                    filter(orm.and_(orm.or_(orm.uatrains.E.value == et.value, orm.uatrains.E.oid == et.oid),
+                    filter(orm.and_(orm.uatrains.E.oid == et.oid,
                         orm.uatrains.E.ua_title == et.ua_title, orm.uatrains.E.ru_title == et.ru_title,
-                        orm.or_(orm.uatrains.E.etype == 1, orm.uatrains.E.etype == 4))).all()
+                        orm.uatrains.E.etype == 5)).all()
                 if len(similar_etrains) > 1:
                     for similar_et in similar_etrains:
-                        if (similar_et.from_date is not None and similar_et.to_date is not None and \
-                                similar_et.from_date <= datetime.datetime.now() and \
-                                similar_et.to_date >= datetime.datetime.now()) and \
-                            ((last_etrain.from_date is None and last_etrain.to_date is None) or \
-                                (last_etrain.to_date <= similar_et.from_date) or \
-                                (last_etrain.id < similar_et.id)):
+                        if similar_etrains.c_date > last_etrain.c_date:
                             last_etrain = similar_et
                 if last_etrain.id > et.id:
                     et.ref_id = last_etrain.id
@@ -39,10 +34,10 @@ try:
     conn.close()
     end_time = time.time()
     exec_delta = datetime.timedelta(seconds=int(end_time - start_time))
-    exec_log.info('uatrains bot etrain referer %s %s' % (str(start_dt), str(exec_delta)))
+    exec_log.info('uatrains bot ptrain referer %s %s' % (str(start_dt), str(exec_delta)))
 except:
     if ses is not None:
         ses.close()
     if conn is not None:
         conn.close()
-    nlog.info('uatrains bot - etrain referer fatal', traceback.format_exc())
+    nlog.info('uatrains bot - ptrain referer fatal', traceback.format_exc())
