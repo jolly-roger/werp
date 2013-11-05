e10c3e80e8d06e1b72a16f93699949862c3e132f
roger <ionyshchenko@sequans.com>
2013-11-05 18:05:28 +0200

    i

--- dap/layout/templates/js/script.js
+++ dap/layout/templates/js/script.js
@@ -22,16 +22,16 @@ $(function(){
     }
     
     $('#check_proxies_btn').click(function(){
-        is_valid = false;
+        is_valid = true;
         domain_value = $('#domain_value').val()
         
         if(typeof(domain_value) == 'undefined' || domain_value == ''){
             alert('You should specify domain name or URL');
-            return;
+            is_valid = false;
         }
         if(domain_value.match(/^http:\/\//gi) || domain_value.match(/^https:\/\//gi)){
             alert('Domain name or URL shouldn\'t start with "http://" or "https://"');
-            return;
+            is_valid = false;
         }
         
         if(is_valid){
