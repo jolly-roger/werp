b1ccacbf9e08b830190bc8d851da6df168fe5e0e
roger <ionyshchenko@sequans.com>
2013-11-18 14:40:52 +0200

    i

--- dap/layout/templates/js/check.js
+++ dap/layout/templates/js/check.js
@@ -1,13 +1,13 @@
 var proxy_status_prefix = 'proxy_status_'
 
 function get_accessibility(){
-    $.getJSON( "/get_checked?ses_key=" + ses_key + "&domain=" + domain, function(data){
+    $.getJSON( "/check/get_checked?ses_key=" + ses_key + "&domain=" + domain, function(data){
         for (var i in data) {
             $('#' + proxy_status_prefix + data[i]['id']).
                 html('<span class="round label accessible_status">{% trans %}accessible_label{% endtrans %}</span>');
         }
     });
-    $.getJSON( "/is_finished?ses_key=" + ses_key + "&domain=" + domain, function(data){
+    $.getJSON( "/check/is_finished?ses_key=" + ses_key + "&domain=" + domain, function(data){
         if(!data){
             setTimeout(get_accessibility, 1000);
         }else{
@@ -19,13 +19,11 @@ function get_accessibility(){
     });
 }
 
-$.post('/run', {ses_key: ses_key, domain: domain, jproxies: jproxies}, function(data){
-    for (var i in proxies) {
-        proxy_status_td = $('#' + proxy_status_prefix + proxies[i]['id']);
-        proxy_status_td.
-            html('<span class="round label checking_status">{% trans %}checking_label{% endtrans %}</span>' +
-                '<img class="checking_progress" src="/images/checking.gif" alt="..." />');
-    }
-});
+for (var i in proxies) {
+    proxy_status_td = $('#' + proxy_status_prefix + proxies[i]['id']);
+    proxy_status_td.
+        html('<span class="round label checking_status">{% trans %}checking_label{% endtrans %}</span>' +
+            '<img class="checking_progress" src="/images/checking.gif" alt="..." />');
+}
 
 setTimeout(get_accessibility, 1000);
