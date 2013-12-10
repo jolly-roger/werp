669cdc085408f59bf6f500049a035bc1eb001019
roger <ionyshchenko@sequans.com>
2013-12-10 12:50:01 +0200

    i

--- orm/ukrainianside/article.py
+++ orm/ukrainianside/article.py
@@ -7,14 +7,11 @@ class Article(DBase):
     id = Column(BigInteger, primary_key = True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    content = Column(String)
     order = Column(Integer, nullable=False)
     alias = Column(String, nullable=False)
     
-    def __init__(self, name=None, description=None, content=None, order=None):
+    def __init__(self, name=None, description=None, order=None):
         self.title = title
         self.description = description
-        self.content = content
         self.order = order
-        self.url = url
         self.alias = alias
