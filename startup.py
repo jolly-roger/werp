e43538a6a9af513205486f4e0384fabf434879b2
roger <ionyshchenko@sequans.com>
2014-04-28 16:19:44 +0300

    i

--- startup.py
+++
@@ -1,8 +0,0 @@
-import os
-import os.path
-
-from werp.common import sockets
-
-
-if os.path.isfile(sockets.get_socket_path(sockets.froxly_grabber_server)):
-    os.remove(sockets.get_socket_path(sockets.froxly_grabber_server))
