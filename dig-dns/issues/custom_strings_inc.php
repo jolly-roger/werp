39fa5c9c1b0bdfb00134ed632905934c97093cea
roger <ionyshchenko@sequans.com>
2014-09-25 14:04:08 +0300

    i

--- dig-dns/issues/custom_strings_inc.php
+++ dig-dns/issues/custom_strings_inc.php
@@ -1,3 +1,5 @@
 <?php
     $s_status_enum_string = '10:new,15:in progress,20:feedback,30:acknowledged,40:confirmed,50:assigned,80:resolved,90:closed';
+    $s_in_progress_bug_title = 'In Progress';
+    //$s_in_progress_bug_button = 'In Progress';
 ?>
