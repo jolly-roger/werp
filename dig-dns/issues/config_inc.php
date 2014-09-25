bed26e4836c44a492ea295de4d462ce01296421a
www <www@dig-dns.com>
2014-09-25 13:45:26 +0300

    i

---
+++ dig-dns/issues/config_inc.php
@@ -0,0 +1,7 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysqli';
+	$g_database_name = 'issues';
+	$g_db_username = 'www';
+	$g_db_password = '[5h$V6tS';
+?>
---
+++ dig-dns/wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+roger:$1$Kw0XJevU$RNKJ3JAZ5mTu.Rbe4VESn/:igor:igor.onyshchenko@gmail.com:admin,user
