16df7cf83b1784d8c91de9d397f93af9e5b845ee
roger <ionyshchenko@sequans.com>
2014-09-25 16:08:48 +0300

    i

---
+++ dig-dns/sql/mysql_dovecot_users.sql
@@ -0,0 +1,10 @@
+create view dovecot_users as
+select (u.name || '@' || d."domain") as "user",
+    ('/home/mailer/' || md.path) AS home,
+    ('maildir:/home/mailer/' || md.path) AS mail,
+    5003 AS uid,
+    5003 AS gid
+from mailboxes as mb
+inner join users as u on mb.user_id = u.id
+inner join domains as d on mb.domain_id = d.id
+inner join maildirs md ON mb.maildir_id = md.id;
---
+++ dig-dns/sql/mysql_mailboxes.sql
@@ -0,0 +1,24 @@
+--drop table mailboxes;
+create table mailboxes (id bigint not null auto_increment, user_id bigint not null, domain_id bigint not null, maildir_id bigint not null, primary key (id));
+
+-- maildir_id | domain_id | user_id | id 
+--------------+-----------+---------+----
+--          3 |         0 |       3 | 14
+--          3 |         7 |       3 | 21
+--          3 |        10 |       3 | 24
+--          4 |         0 |       6 | 47
+--          4 |         7 |       6 | 54
+--          4 |        10 |       6 | 57
+--          5 |         0 |       7 | 58
+--          5 |         7 |       7 | 65
+--          5 |        10 |       7 | 68
+
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (1, 1, 1);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (1, 2, 1);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (1, 3, 1);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (2, 1, 2);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (2, 2, 2);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (2, 3, 2);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (3, 1, 3);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (3, 2, 3);
+insert into mailboxes (user_id, domain_id, maildir_id) valuse (3, 3, 3);
