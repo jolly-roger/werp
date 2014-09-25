044ae2d3d4773448b9442072044d064e001cf8bc
roger <ionyshchenko@sequans.com>
2014-09-25 15:39:59 +0300

    i

---
+++ dig-dns/sql/mysql_domains.sql
@@ -0,0 +1,6 @@
+drop table domains;
+create table domains (id bigint not null auto_increment, domain varchar(256) not null, primary key (id));
+
+insert into domains (domain) values ('dig-dns.com');
+insert into domains (domain) values ('uatrains.com');
+insert into domains (domain) values ('ukrainianside.com');
