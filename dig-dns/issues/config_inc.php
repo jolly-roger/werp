0bb5dfc4dc0cbc9388200348635ebe122d20b66c
roger <ionyshchenko@sequans.com>
2014-09-25 13:50:24 +0300

    i

--- dig-dns/issues/config_inc.php
+++ dig-dns/issues/config_inc.php
@@ -4,4 +4,6 @@
 	$g_database_name = 'issues';
 	$g_db_username = 'www';
 	$g_db_password = '[5h$V6tS';
+	
+	$g_status_enum_string = '10:new,15:in progress,20:feedback,30:acknowledged,40:confirmed,50:assigned,80:resolved,90:closed';
 ?>
