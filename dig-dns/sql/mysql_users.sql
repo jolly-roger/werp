5ee6d71e39cd340d406f55f80c98766b4fa63240
roger <ionyshchenko@sequans.com>
2014-09-25 15:34:10 +0300

    i

--- dig-dns/sql/mysql_users.sql
+++ dig-dns/sql/mysql_users.sql
@@ -1,4 +1,5 @@
-create table users (id bigint not null , name varchar(256) not null, password varchar(256) not null, primary key (id));
+drop table users;
+create table users (id bigint not null auto_increment, name varchar(256) not null, password varchar(256) not null, primary key (id));
 
 insert into users (name, password) values ('roger', '23ee0084cb55f5f0c6e5ff3b8f0efa4cc1c2dea9f4e5e2551543dacdb67375f1');
 insert into users (name, password) values ('anna', '23ee0084cb55f5f0c6e5ff3b8f0efa4cc1c2dea9f4e5e2551543dacdb67375f1');
